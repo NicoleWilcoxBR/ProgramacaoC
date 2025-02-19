a3428351b8148596d4534d40c96112d42ed8dd18
Nicole Wilcox <nicolewilcox@outlook.com.br>
2025-02-19 07:46:52 -0300

    Incluindo primeiro script Ola Mundo no meu repositorio da faculdade Estacio

---
+++ hello.c
@@ -0,0 +1,12 @@
+/* Esse programa tem uma biblioteca padrão de input e output*/
+#include <stdio.h>
+
+/*Funcao principal todo programa tem um*/
+int main(){
+
+    /*Comando de output saida de mensagem na tela*/
+    printf("Ola, Mundo!\n");
+
+    /*Todo programa que retorna um valor inteiro*/
+    return 0;
+}
