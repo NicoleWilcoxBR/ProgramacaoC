d5c11e83fb6b7164bbff439caa870af8a9143e9e
Nicole Wilcox <nicolewilcox@outlook.com.br>
2025-02-19 10:16:44 -0300

    Variaveis adicionada no github

---
+++ variaveis.c
@@ -0,0 +1,14 @@
+/* Biblioteca padrao de entrada e saida */
+#include <stdio.h>
+
+/* Funcao principal do programa*/
+int main(){
+    int idade; //Variavel do tipo inteiro
+    int quantidade; //Variavel do tipo inteiro
+    float altura; //Variavel de ponto flutuante decima
+    char letra; //Variavel de caracteres
+    char nome[30]; //Variavel de texto recebe nomes
+    double peso;
+
+    return 0; //Fim do programa
+}
