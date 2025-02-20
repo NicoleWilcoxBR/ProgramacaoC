1886a0da8e22eb93a0b8833e799d0d5e6bc21266
Nicole Wilcox <nicolewilcox@outlook.com.br>
2025-02-20 09:51:27 -0300

    aprendendo sobre modularizacao

---
+++ modularizacao.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void entradaDeDados(){
+
+}
+
+float calcularMedia(){
+
+}
+
+float exibirResultados(){
+
+}
+
+int main(){
+    float media;
+
+    entradaDeDados();
+
+    media = calcularMedia();
+
+    exibirResultados();
+
+    return 0;
+}
